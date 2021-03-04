ad87008c1fecbef9ded42de4788cb475173033b9
akashmadhu38 <76092167+akashmadhu38@users.noreply.github.com>
2021-03-04 21:04:51 +0530

    Update CO1 PG 3.c.py

--- CO1 PG 3.c.py
+++ CO1 PG 3.c.py
@@ -1,5 +1,5 @@
 w=input("Enter a word")
 v=["a","e","i","o","u"];
-for i in range(1,len(w)):
-    if w[i]=="a" or w[i]=="e" or w[i]=="i" or w[i]=="o" or w[i]=="u" or w[i]=="A" or w[i]=="E" or w[i]=="I" or w[i]=="O" or w[i]=="U":
-        print(i,w[i] )
+s=[i for i in w if i in v]
+print("Number of vowels:",len(s))
+print(s)
